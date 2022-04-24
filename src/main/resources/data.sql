0361e420e68eabc5b0c8d7d8474868f16dba83ad
dvsradadiya <dradadiya0002@gmail.com>
2022-04-24 16:21:17 +0530

    data.sql added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+DELETE FROM ngo;
+INSERT INTO ngo (`ngo_id`,`address1`, `address2`, `city`, `details`, `email_id`, `mobile`, `name`, `pin_code`) VALUES (1,'yamuna park', 'kamrej', 'surat', 'Food NGO', 'dradadiya990@gmail.com', '9909859936', 'Yamuna', '395005');
+INSERT INTO ngo (`ngo_id`,`address1`, `address2`, `city`, `details`, `email_id`, `mobile`, `name`, `pin_code`) VALUES (2,'Lakhan park', 'citylight', 'surat', 'NGO', 'dradadiya0002@gmail.com', '9773180438', 'Narayan', '395006');
