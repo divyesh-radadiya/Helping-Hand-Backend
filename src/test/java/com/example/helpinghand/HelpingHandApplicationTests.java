1853fef7678d576e7b4e6f2ea3b91c5c2890b966
dvsradadiya <dradadiya0002@gmail.com>
2022-05-11 00:24:20 +0530

    Jenkinsfile updated

--- src/test/java/com/example/helpinghand/HelpingHandApplicationTests.java
+++ src/test/java/com/example/helpinghand/HelpingHandApplicationTests.java
@@ -1,13 +1,49 @@
 package com.example.helpinghand;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.jupiter.api.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@EnableWebMvc
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class HelpingHandApplicationTests {
 
+    @Autowired
+    WebApplicationContext webApplicationContext;
+    protected MockMvc mvc;
+    @BeforeEach
+    protected void setUp() throws Exception {
+        this.mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
     @Test
+    @Order(1)
     void contextLoads() {
+    }
+
+    @Test
+    @Order(2)
+    void  getNgoTest() throws Exception
+    {
+//
     }
 
 }
