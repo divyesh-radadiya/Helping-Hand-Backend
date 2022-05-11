aa656e392f322af40eb7aa2ab61e9335392a8b7e
dvsradadiya <dradadiya0002@gmail.com>
2022-05-11 09:33:59 +0530

    Testing updated

--- src/test/java/com/example/helpinghand/HelpingHandApplicationTests.java
+++ src/test/java/com/example/helpinghand/HelpingHandApplicationTests.java
@@ -59,100 +59,100 @@ class HelpingHandApplicationTests extends AbstractTest{
 
     }
 
-    @Test
-    @Order(3)
-    void  requestTest() throws Exception
-    {
-        String req="";
-        try {
-            File file = new File("src\\test\\java\\com\\example\\helpinghand\\req.json");
-            JSONParser parser = new JSONParser();
-            Object obj = parser.parse(new FileReader(file));
-
-        req = obj.toString();
-        } catch (IOException | ParseException e) {
-            System.out.println(e);
-        }
-        String  uri = "/request/add";
-        MvcResult mvcResult = this.mvc.perform(MockMvcRequestBuilders.post(uri)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(req)
-                .accept(MediaType.APPLICATION_JSON)).andReturn();
-        int status = mvcResult.getResponse().getStatus();
-
-        String content = mvcResult.getResponse().getContentAsString();
-        try {
-            assertEquals( 200, status);
-            System.out.println("Register request : Success");
-        }catch(Exception e) {
-            System.out.println("Register request : Fail");
-        }
-
-        uri = "/request/setStatus/1";
-        mvcResult = this.mvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.APPLICATION_JSON)).andReturn();
-        status = mvcResult.getResponse().getStatus();
-        try {
-            assertEquals( 200, status);
-            System.out.println("Set request : Success");
-        }catch(Exception e) {
-            System.out.println("Ser request : Fail");
-        }
-
-        uri = "/request/unsetStatus/1";
-        mvcResult = this.mvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.APPLICATION_JSON)).andReturn();
-        status = mvcResult.getResponse().getStatus();
-        try {
-            assertEquals( 200, status);
-            System.out.println("Unset request : Success");
-        }catch(Exception e) {
-            System.out.println("Unset request : Fail");
-        }
-
-        uri = "/request/getRequests/9999999999";
-        mvcResult = this.mvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.APPLICATION_JSON)).andReturn();
-        status = mvcResult.getResponse().getStatus();
-        try {
-            assertEquals( 200, status);
-            System.out.println("Get request : Success");
-        }catch(Exception e) {
-            System.out.println("Get request : Fail");
-        }
-
-    }
-
-    @Test
-    @Order(4)
-    void  donorTest() throws Exception
-    {
-        String req="";
-        try {
-            File file = new File("src\\test\\java\\com\\example\\helpinghand\\donor.json");
-            JSONParser parser = new JSONParser();
-            Object obj = parser.parse(new FileReader(file));
-
-            req = obj.toString();
-        } catch (IOException | ParseException e) {
-            System.out.println(e);
-        }
-        String  uri = "/donor/add";
-        MvcResult mvcResult = this.mvc.perform(MockMvcRequestBuilders.post(uri)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(req)
-                .accept(MediaType.APPLICATION_JSON)).andReturn();
-        int status = mvcResult.getResponse().getStatus();
-
-        String content = mvcResult.getResponse().getContentAsString();
-        try {
-            assertEquals( 200, status);
-            System.out.println("Register donor : Success");
-        }catch(Exception e) {
-            System.out.println("Register donor : Fail");
-        }
-
-
-    }
+//    @Test
+//    @Order(3)
+//    void  requestTest() throws Exception
+//    {
+//        String req="";
+//        try {
+//            File file = new File("src\\test\\java\\com\\example\\helpinghand\\req.json");
+//            JSONParser parser = new JSONParser();
+//            Object obj = parser.parse(new FileReader(file));
+//
+//        req = obj.toString();
+//        } catch (IOException | ParseException e) {
+//            System.out.println(e);
+//        }
+//        String  uri = "/request/add";
+//        MvcResult mvcResult = this.mvc.perform(MockMvcRequestBuilders.post(uri)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(req)
+//                .accept(MediaType.APPLICATION_JSON)).andReturn();
+//        int status = mvcResult.getResponse().getStatus();
+//
+//        String content = mvcResult.getResponse().getContentAsString();
+//        try {
+//            assertEquals( 200, status);
+//            System.out.println("Register request : Success");
+//        }catch(Exception e) {
+//            System.out.println("Register request : Fail");
+//        }
+//
+//        uri = "/request/setStatus/1";
+//        mvcResult = this.mvc.perform(MockMvcRequestBuilders.get(uri)
+//                .accept(MediaType.APPLICATION_JSON)).andReturn();
+//        status = mvcResult.getResponse().getStatus();
+//        try {
+//            assertEquals( 200, status);
+//            System.out.println("Set request : Success");
+//        }catch(Exception e) {
+//            System.out.println("Ser request : Fail");
+//        }
+//
+//        uri = "/request/unsetStatus/1";
+//        mvcResult = this.mvc.perform(MockMvcRequestBuilders.get(uri)
+//                .accept(MediaType.APPLICATION_JSON)).andReturn();
+//        status = mvcResult.getResponse().getStatus();
+//        try {
+//            assertEquals( 200, status);
+//            System.out.println("Unset request : Success");
+//        }catch(Exception e) {
+//            System.out.println("Unset request : Fail");
+//        }
+//
+//        uri = "/request/getRequests/9999999999";
+//        mvcResult = this.mvc.perform(MockMvcRequestBuilders.get(uri)
+//                .accept(MediaType.APPLICATION_JSON)).andReturn();
+//        status = mvcResult.getResponse().getStatus();
+//        try {
+//            assertEquals( 200, status);
+//            System.out.println("Get request : Success");
+//        }catch(Exception e) {
+//            System.out.println("Get request : Fail");
+//        }
+//
+//    }
+//
+//    @Test
+//    @Order(4)
+//    void  donorTest() throws Exception
+//    {
+//        String req="";
+//        try {
+//            File file = new File("src\\test\\java\\com\\example\\helpinghand\\donor.json");
+//            JSONParser parser = new JSONParser();
+//            Object obj = parser.parse(new FileReader(file));
+//
+//            req = obj.toString();
+//        } catch (IOException | ParseException e) {
+//            System.out.println(e);
+//        }
+//        String  uri = "/donor/add";
+//        MvcResult mvcResult = this.mvc.perform(MockMvcRequestBuilders.post(uri)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(req)
+//                .accept(MediaType.APPLICATION_JSON)).andReturn();
+//        int status = mvcResult.getResponse().getStatus();
+//
+//        String content = mvcResult.getResponse().getContentAsString();
+//        try {
+//            assertEquals( 200, status);
+//            System.out.println("Register donor : Success");
+//        }catch(Exception e) {
+//            System.out.println("Register donor : Fail");
+//        }
+//
+//
+//    }
 
 }
